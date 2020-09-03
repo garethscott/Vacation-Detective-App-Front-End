bdc60dfb88b8d5707e9730b464d8aa0d71773471
LJS22 <LJS22@users.noreply.github.com>
2020-09-03 11:06:15 +0100

    bug fixes for saving users

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,82 +1,107 @@
-import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class SignUpForm extends React.Component {
-    state = {
-        firstName: '',
-        surName: '',
-        lastName: '',
-        email: '',
-        password: ''
-    }
+  state = {
+    firstName: null,
+    lastName: null,
+    userName: null,
+    email: null,
+    password: null,
+  };
 
-    handleChange = async (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleFirstName = (e) => {
+    this.setState({
+      firstName: e.target.value,
+    });
+    console.log(this.state.firstName);
+  };
 
-    handleSubmit = async (e) => {
-        e.preventDefault()
-        const newForm = {
-            firstName: this.state.firstName,
-            lastName: this.state.surname,
-            userName: this.state.username,
-            email: this.state.email,
-            password: this.state.password
-        }
-        await axios.post('http://localhost:4500/vacationdetective/v1/addUser', newForm)
-            .then(response => {
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        this.setState({
-            firstName: '',
-            surname: '',
-            username: '',
-            email: '',
-            password: ''
-        })
+  handleLastName = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
 
-    }
+  handleUserName = (e) => {
+    this.setState({
+      userName: e.target.value,
+    });
+  };
 
-    render() {
-        // console.log(this.state.firstName)
-        // console.log(this.state.surname)
-        // console.log(this.state.username)
-        // console.log(this.state.email)
-        // console.log(this.state.password)
-        return (
-            <div className="signup-form-box">
-                <h1>Please fill out form below</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="main-form-box">
-                        <div className="left-form-box">
-                            <h3>Name:</h3>
-                            <input type="text" name="firstName" onChange={this.handleChange} />
-                            <h3>Surname:</h3>
-                            <input type="text" name="lastName" onChange={this.handleChange} />
-                            <h3>Username:</h3>
-                            <input type="text" name="userName" onChange={this.handleChange} />
-                        </div>
-                        <div className="right-form-box">
-                            <h3>Email:</h3>
-                            <input type="text" name="email" onChange={this.handleChange} />
-                            <h3>Password:</h3>
-                            <input type="text" name="password" onChange={this.handleChange} />
-                            <Link to="/home"><h3>Home</h3></Link>
-                        </div>
-                    </div>
-                    <button className="submit-signup-btn" type="submit">Submit</button>
-                </form>
+  handleEmail = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
 
+  handlePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleSubmit = async (e) => {
+    const response = await fetch(
+      "http://localhost:4500/vacationdetective/v1/addUser",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          userName: this.state.userName,
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      }
+    );
+    e.preventDefault();
+    const data = await response.json();
+    console.log(data);
+  };
+
+  render() {
+    // console.log(this.state.firstName)
+    // console.log(this.state.surname)
+    // console.log(this.state.username)
+    // console.log(this.state.email)
+    // console.log(this.state.password)
+    return (
+      <div className="signup-form-box">
+        <h1>Please fill out form below</h1>
+        <form>
+          <div className="main-form-box">
+            <div className="left-form-box">
+              <h3>Name:</h3>
+              <input type="text" onChange={this.handleFirstName} />
+              <h3>Surname:</h3>
+              <input type="text" onChange={this.handleLastName} />
+              <h3>Username:</h3>
+              <input type="text" onChange={this.handleUserName} />
             </div>
-        )
-    }
+            <div className="right-form-box">
+              <h3>Email:</h3>
+              <input type="text" onChange={this.handleEmail} />
+              <h3>Password:</h3>
+              <input type="text" onChange={this.handlePassword} />
+              <Link to="/home">
+                <h3>Home</h3>
+              </Link>
+            </div>
+          </div>
+          <button
+            className="submit-signup-btn"
+            type="submit"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignUpForm;
