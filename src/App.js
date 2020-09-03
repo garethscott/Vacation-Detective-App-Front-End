18baca9abc29fde5372338ee934789c88d7c45b8
Joseph D'ambra <josephdambra@josephs-mbp.home>
2020-09-03 11:08:11 +0100

    tickboxes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import LandingPage from './components/LandingPage'
-import SignUpForm from './components/SignUpForm'
-
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import SignUpForm from "./components/SignUpForm";
+import Questions from "./components/Questions";
 
 class App extends React.Component {
-  state = {}
+  state = {};
   render() {
     return (
       <BrowserRouter>
@@ -15,6 +14,7 @@ class App extends React.Component {
           <div className="wrapper">
             <Route path="/home" component={LandingPage} />
             <Route path="/signup" component={SignUpForm} />
+            <Route path="/questions" component={Questions} />
           </div>
         </div>
       </BrowserRouter>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,35 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-// import 'LandingPage.css';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class LandingPage extends React.Component {
-    state = {
-        email: '',
-        password: ''
-    }
+  state = {
+    email: "",
+    password: "",
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    render() {
-        console.log(this.state.email)
-        console.log(this.state.password)
-        return (
-            <div className="signin-content-box">
-                <h1>Welcome to Vacation Detective</h1>
-                <form>
-                    <h3>Email:</h3>
-                    <input type="text" name="email" onChange={this.handleChange} />
-                    <h3>Password:</h3>
-                    <input type="password" name="password" onChange={this.handleChange} /><br />
-                    <button className="submit-btn" type="submit">Submit</button>
-                    <Link to="/signup" ><h3>Sign up</h3></Link>
-                </form>
-            </div>
-        )
-    }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    console.log(this.state.email);
+    console.log(this.state.password);
+    return (
+      <div className="signin-content-box">
+        <h1>Welcome to Vacation Detective</h1>
+        <form>
+          <h3>Email:</h3>
+          <input type="text" name="email" onChange={this.handleChange} />
+          <h3>Password:</h3>
+          <input type="password" name="password" onChange={this.handleChange} />
+          <br />
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
+          <Link to="/signup">
+            <h3>Sign up</h3>
+          </Link>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
